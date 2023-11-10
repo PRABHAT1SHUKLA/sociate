6feb07fa8ee6762952d46d2ce4998401a8dc184b
Pratham Sahu <prathamsahu051@gmail.com>
2023-11-11 02:40:47 +0530

    Added comment

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -56,6 +56,7 @@ const Layout = async ({ children }: LayoutProps) => {
       <div className="md:hidden">
         <MobileChatLayout friends={friends} session={session} sidebarOptions={sidebarOptions} unseenRequestCount={unseenRequestCount} />
       </div>
+      {/* Desktop view */}
       <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 truncate border-r border-gray-200 bg-white px-6">
         <Link href="/dashboard" className="flex h-16 shrink-0 items-center">
           <Icons.Logo className="h-8 w-auto text-indigo-600" />
