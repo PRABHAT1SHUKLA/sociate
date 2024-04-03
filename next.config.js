f5044b729fdbcb224fd4319d8910b34e76557ea5
Pratham Sahu <prathamsahu051@gmail.com>
2024-04-03 18:54:43 +0530

    Updated configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+    domains: ["uploadthing.com", "lh3.googleusercontent.com"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
